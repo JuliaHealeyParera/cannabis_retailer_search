5c14aa5506fd411601d19a67e177fd00be374f7c
Mike Dolan Fliss <mike.dolan.fliss@gmail.com>
2025-05-06 10:10:21 -0400

    initial code scratchpad example

---
+++ code/Mike scratchpad/mike_initial_scratchpad.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(sf)
+library(readxl)
+
+tobacco_tbl = read_csv("ignore/data/tobacco retailers/list3_tobacco_20240910_pregeocode_geocodio_6e3531302d300449332adfded219f5770d2cbd95.csv")
+tobacco_tbl
